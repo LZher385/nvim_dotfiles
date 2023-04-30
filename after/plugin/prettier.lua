2efad130eeb4ac68a0df56e94da085d8d182b2d1
Yu Zher Lim <ylim@armorblox.com>
2023-04-29 23:15:25 -0700

    add plugins config

---
+++ after/plugin/prettier.lua
@@ -0,0 +1,11 @@
+local status, prettier = pcall(require, "pretter")
+if (not status) then return end
+
+prettier.setup {
+    bin = 'prettierd',
+    filetypes = {
+        "css",
+        "javascript",
+        "javascriptreact"
+    }
+}
